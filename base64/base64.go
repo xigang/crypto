1a73d3b5a89628fc5f70b972fafd8ded5ebf43b8
wangxigang <wangxigang@le.com>
2017-06-14 10:48:48 +0800

    add base64 URL

--- base64/base64.go
+++ base64/base64.go
@@ -4,12 +4,24 @@ import (
 	"encoding/base64"
 )
 
-//Base64Encode return Base64 encrypted data
-func Base64Encode(src []byte) string {
+//Base64EncodeString return Base64 encrypted data
+func Base64EncodeString(src []byte) string {
 	return base64.StdEncoding.EncodeToString(src)
 }
 
-//Base64Decode return Base64 decryption data
-func Base64Decode(src string) ([]byte, error) {
+//Base64DecodeString return Base64 decryption data
+func Base64DecodeString(src string) ([]byte, error) {
 	return base64.StdEncoding.DecodeString(src)
 }
+
+//Base64EncodeURL returns the base64 encoding of src.
+func Base64EncodeURL(src []byte) string {
+	return base64.URLEncoding.EncodeToString(src)
+}
+
+//Base64DecodeURL returns the bytes represented by the base64 string s.
+func Base64DecodeURL(src string) ([]byte, error) {
+	return base64.URLEncoding.DecodeString(src)
+}
+
+//TODO User-defined encryption.
