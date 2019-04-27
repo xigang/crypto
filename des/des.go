684c80b6005d2630d7049319a918898360086c23
xigang <wangxigang2014@gmail.com>
2019-04-27 20:25:25 +0800

    fix DES/TDES uses key as an IV issue

--- des/des.go
+++ des/des.go
@@ -1,62 +1,75 @@
 package des
 
 import (
+	"bytes"
 	"crypto/cipher"
 	"crypto/des"
-
-	"github.com/xigang/crypto"
 )
 
-//DesEncrypt return DES encryption
-func DesEncrypt(origData []byte, key []byte) ([]byte, error) {
+func DesEncryption(key, iv, plainText []byte) ([]byte, error) {
 	block, err := des.NewCipher(key)
 	if err != nil {
 		return nil, err
 	}
 
-	origData = crypto.PKCS5Padding(origData, block.BlockSize())
-	blockMode := cipher.NewCBCEncrypter(block, key)
-	crypted := make([]byte, len(origData))
-	blockMode.CryptBlocks(crypted, origData)
-	return crypted, nil
+	blockSize := block.BlockSize()
+	origData := PKCS5Padding(plainText, blockSize)
+	blockMode := cipher.NewCBCEncrypter(block, iv)
+	cryted := make([]byte, len(origData))
+	blockMode.CryptBlocks(cryted, origData)
+	return cryted, nil
 }
 
-//DesDecrypt return DES decryption
-func DesDecrypt(crypted []byte, key []byte) ([]byte, error) {
+func DesDecryption(key, iv, cipherText []byte) ([]byte, error) {
 	block, err := des.NewCipher(key)
 	if err != nil {
 		return nil, err
 	}
 
-	blockMode := cipher.NewCBCDecrypter(block, key)
-	origData := make([]byte, len(crypted))
-	blockMode.CryptBlocks(origData, crypted)
-	origData = crypto.PKCS5UnPadding(origData)
+	blockMode := cipher.NewCBCDecrypter(block, iv)
+	origData := make([]byte, len(cipherText))
+	blockMode.CryptBlocks(origData, cipherText)
+	origData = PKCS5UnPadding(origData)
 	return origData, nil
 }
 
-//TripleDesEncrypt return 3DES encryption
-func TripleDesEncrypt(origData, key []byte) ([]byte, error) {
+func PKCS5Padding(ciphertext []byte, blockSize int) []byte {
+	padding := blockSize - len(ciphertext)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(ciphertext, padtext...)
+}
+
+func PKCS5UnPadding(origData []byte) []byte {
+	length := len(origData)
+	unpadding := int(origData[length-1])
+	return origData[:(length - unpadding)]
+}
+
+func TripleDesEncrypt(data, key []byte) ([]byte, error) {
 	block, err := des.NewTripleDESCipher(key)
 	if err != nil {
 		return nil, err
 	}
-	origData = crypto.PKCS5Padding(origData, block.BlockSize())
-	blockMode := cipher.NewCBCEncrypter(block, key[:8])
-	crypted := make([]byte, len(origData))
-	blockMode.CryptBlocks(crypted, origData)
-	return crypted, nil
+	ciphertext := key
+	iv := ciphertext[:des.BlockSize]
+	origData := PKCS5Padding(data, block.BlockSize())
+	mode := cipher.NewCBCEncrypter(block, iv)
+	encrypted := make([]byte, len(origData))
+	mode.CryptBlocks(encrypted, origData)
+	return encrypted, nil
 }
 
-//TripleDesDecrypt return 3DES decryption
-func TripleDesDecrypt(crypted, key []byte) ([]byte, error) {
+func TripleDesDecrypt(data, key []byte) ([]byte, error) {
 	block, err := des.NewTripleDESCipher(key)
 	if err != nil {
 		return nil, err
 	}
-	blockMode := cipher.NewCBCDecrypter(block, key[:8])
-	origData := make([]byte, len(crypted))
-	blockMode.CryptBlocks(origData, crypted)
-	origData = crypto.PKCS5UnPadding(origData)
-	return origData, nil
+	ciphertext := key
+	iv := ciphertext[:des.BlockSize]
+
+	decrypter := cipher.NewCBCDecrypter(block, iv)
+	decrypted := make([]byte, len(data))
+	decrypter.CryptBlocks(decrypted, data)
+	decrypted = PKCS5UnPadding(decrypted)
+	return decrypted, nil
 }
